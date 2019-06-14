c1ba618a5c7a68f11ab3f5dd5bb00a85d950a5b2
Albert Chung <chung972@gmail.com>
2019-06-14 11:56:37 -0500

    creates path for add_skill

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-
+    path('add_skill/', views.CreateSkill.as_view(), name="add_skill"),
 ]
