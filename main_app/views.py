9fa2ca6005d2f810e487d6bdc168f743224ddf62
Albert Chung <chung972@gmail.com>
2019-06-14 11:06:13 -0500

    adds view functions for signup, login_view, and logout_view; imports required modules

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from .forms import LoginForm
 
 # Create your views here.
 
@@ -7,3 +10,46 @@ def home(request):
     return render(request, 'home.html')
     # like how render behaves in express, render in django also looks for
     # files RELATIVE to the templates directory
+
+
+def signup(request):
+    # need to refactor to post form to db
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+
+    form = UserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'signup.html', context)
+
+
+def login_view(request):
+    err_msg = ''
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            # Authenticate the User
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                err_msg = 'Sorry, your username or password was invalid.'
+    form = LoginForm()
+    context = {
+        'form': form,
+        'err_msg': err_msg,
+    }
+    return render(request, 'login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
